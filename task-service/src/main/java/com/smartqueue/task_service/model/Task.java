c2975ada8e6fb8a8219470156b042566cb90d5a5
Vishal22598 <vishalgupta22598@gmail.com>
2026-04-30 15:56:50 +0530

    Task class created with required states and behavior

--- task-service/src/main/java/com/smartqueue/task_service/model/Task.java
+++ task-service/src/main/java/com/smartqueue/task_service/model/Task.java
@@ -1,5 +1,35 @@
 package com.smartqueue.task_service.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Data
 public class Task {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private String taskType;
+
+    @Column(columnDefinition = "TEXT")
+    private String payload;
+
+    private Integer priority = 1;
+
+    private String status = "PENDING";
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    @Column(columnDefinition = "TEXT")
+    private String result;
 }

6bbf4180635548e4d7361a8158d43c391a51efba
Vishal22598 <vishalgupta22598@gmail.com>
2026-04-30 15:59:10 +0530

    TaskRepository interface created with required behavior

--- task-service/src/main/java/com/smartqueue/task_service/repository/TaskRepository.java
+++ task-service/src/main/java/com/smartqueue/task_service/repository/TaskRepository.java
@@ -1,5 +1,14 @@
 package com.smartqueue.task_service.repository;
 
-public class TaskRepository {
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.smartqueue.task_service.model.Task;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUserId(String userId);
+    List<Task> findByStatus(String status);
 }
